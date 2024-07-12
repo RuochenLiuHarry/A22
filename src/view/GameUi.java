5d18b58f980a334aa479924eb010e77424ae2674
RuochenLiuHarry <14575harry@gmail.com>
2024-07-11 23:46:26 -0400

    board

--- src/view/GameUi.java
+++ src/view/GameUi.java
@@ -2,6 +2,8 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,6 +13,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
 public class GameUi extends JFrame {
     private JMenu gameMenu;
@@ -22,6 +26,9 @@ public class GameUi extends JFrame {
     private JMenuItem exitItem;
     private JMenuItem englishItem;
     private JMenuItem chineseItem;
+    private JPanel gridPanel;
+    private JButton[][] gridButtons;
+    private JButton[][] computerGridButtons;
     
     public GameUi() {
     	initiallizeUI();
@@ -65,8 +72,37 @@ public class GameUi extends JFrame {
 	     JLabel logoLabel = new JLabel(logoIcon);
 	     panel.add(logoLabel, BorderLayout.NORTH);
 	        
-	        
+	     gridPanel = new JPanel(new GridLayout(11, 11, 2, 2));
+	     gridPanel.setBackground(new Color(51, 204, 255));   
 	     
+	     gridButtons = new JButton[10][10];
+	     computerGridButtons = new JButton[10][10];
+	        String[] colLabel = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+	        gridPanel.add(new JLabel("")); // Empty top-left corner
+	        for (String label : colLabel) {
+	            gridPanel.add(new JLabel(label, SwingConstants.CENTER));
+	        }
+
+	        for (int i = 0; i < 10; i++) {
+	            gridPanel.add(new JLabel(String.valueOf(i + 1), SwingConstants.CENTER));
+	            for (int j = 0; j < 10; j++) {
+	                gridButtons[i][j] = new JButton();
+	                gridButtons[i][j].setPreferredSize(new Dimension(40, 40));
+	                gridButtons[i][j].setBackground(new Color(51, 204, 255));
+	                gridButtons[i][j].setBorder(new LineBorder(Color.WHITE));
+	                gridPanel.add(gridButtons[i][j]);
+
+	                computerGridButtons[i][j] = new JButton();
+	                computerGridButtons[i][j].setPreferredSize(new Dimension(40, 40));
+	                computerGridButtons[i][j].setBackground(new Color(51, 204, 255));
+	                computerGridButtons[i][j].setBorder(new LineBorder(Color.WHITE));
+	            }
+	        }
+
+	        panel.add(gridPanel, BorderLayout.CENTER);
+	        
+	        
 	     this.add(panel);
          pack();
 	}

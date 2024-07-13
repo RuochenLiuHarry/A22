261448e7833439a245fc2e9c72d63bb85328d4f2
RuochenLiuHarry <14575harry@gmail.com>
2024-07-13 18:29:00 -0400

    delete viewController

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,12 +1,12 @@
 package test;
 
 import view.GameUi;
-import controller.GameController;
+import controller.Controller;
 
 public class Main {
     public static void main(String[] args) {
         GameUi gameUi = new GameUi();
-        GameController gameController = new GameController(gameUi);
+        Controller gameController = new Controller(gameUi);
         gameUi.showMenu();
     }
 }
